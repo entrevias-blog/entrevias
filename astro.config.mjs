d117b888163d74262b8738607e46b5aa0a285f84
zairae <zespinoza@smu.edu>
2026-08-19 17:40:18 -0500

    Prepare Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://entrevias.blog',
-  output: 'server',
-  adapter: cloudflare(),
 });
